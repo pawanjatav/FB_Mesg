1b9553dc998881d5654e373f85162481b16dc33e
pawanjatav <senderfeedback@gmail.com>
2017-05-02 21:05:20 +0530

    Create Echo Bot

--- index.js
+++ index.js
@@ -32,31 +32,68 @@ app.get('/webhook', function (req, res) {
     //if (req.query['hub.verify_token'] === 'testbot_verify_token') {
     //    res.send(req.query['hub.challenge']);
     //} else {
-    console.log(process.env.MESSENGER_PAGE_ACCESS_TOKEN);
+ 
     //}
 
-    var events = req.body.entry[0].messaging;
-    res.send(events.length);
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, { text: "Echo: " + event.message.text });
-          res.send("Echo: Sender ID :: " +event.sender.id);
-        }
+    //var events = req.body.entry[0].messaging;
+    //res.send(events.length);
+    //for (i = 0; i < events.length; i++) {
+    //    var event = events[i];
+    //    if (event.message && event.message.text) {
+    //        sendMessage(event.sender.id, { text: "Echo: " + event.message.text });
+    //      res.send("Echo: Sender ID :: " +event.sender.id);
+    //    }
+    //}
+    //res.sendStatus(200);
+
+
+    var data = req.body;
+    res.send("Echo: page :: " + data.object);
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function (pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+            res.send("Echo: pageID :: " + pageID);
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function (messagingEvent) {
+                //if (messagingEvent.optin) {
+                //    receivedAuthentication(messagingEvent);
+                //} else if (messagingEvent.message) {
+                //    receivedMessage(messagingEvent);
+                //} else if (messagingEvent.delivery) {
+                //    receivedDeliveryConfirmation(messagingEvent);
+                //} else if (messagingEvent.postback) {
+                //    receivedPostback(messagingEvent);
+                //} else {
+                //    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                //}
+
+                res.send("Echo: Sender ID :: " + messagingEvent.sender.id);
+
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know you've 
+        // successfully received the callback. Otherwise, the request will time out.
+        //  res.sendStatus(200);
     }
-    res.sendStatus(200);
 
 });
 
-
+// process.env.PAGE_ACCESS_TOKEN 
 function sendMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        qs: { access_token: 'EAADd0UXT6XEBAFlzejSOzHxb0Q0ZCVW5GRu1cfAeiNWcQ85ymXSVFpf2tA91K0BEqw3cHTBzZAGb3u68Xjry4qcuAQQfWX1isMXDPARyarZAGzFzcoCXvmXESYco4u2hqCNZAOiHJjEMnflS0bt5bEZCxe9A1qfUR3oVTUZAeiETqbCWcMHVSM' },
         method: 'POST',
         json: {
-            recipient: { id: recipientId },
-            message: message,
+            recipient: { id: '197185364124346' },
+            message: { text: 'this is test message for bots' },
         }
     }, function (error, response, body) {
         if (error) {
